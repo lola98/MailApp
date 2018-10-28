873e0c3ff648ae1bc36a74cdddc59bf93658c316
Lola Liu <lola@Lolas-MBP.attlocal.net>
2018-10-27 21:38:26 -0700

    updated

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # MailApp
 
-Description: 
+## Developer
+Lola Liu, Zixin Shen
+
+## Description
+MailApp is an ios application that trakcs packages and informs users of their packages' arrivals. It collects and stores information about all shipments added by users in one single app. 
+
+## Dependencies
+
+
+
+
---
+++ source/source/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  source
+//
+//  Created by SZX on 27/10/2018.
+//
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
